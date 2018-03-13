9603a7f0d80165f7c302152372ebeaf81beccef4
grzegorz <g.jaworski@itlabs.pl>
2018-03-13 21:21:38 +0100

    scheduler

--- src/AppBundle/Controller/EasyAdmin/EmployeeSheduleController.php
+++ src/AppBundle/Controller/EasyAdmin/EmployeeSheduleController.php
@@ -12,25 +12,35 @@ namespace AppBundle\Controller\EasyAdmin;
 use AppBundle\Entity\EmployeesShedule;
 use AppBundle\Entity\User;
 use AppBundle\Entity\VisitDate;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class EmployeeSheduleController extends AdminController
 {
     /**
      * @Route("shedule", name="employees_shedule")
      * @throws \Doctrine\ORM\ORMException
+     * @Method("GET")
      */
-    public function employeeShedule()
+    public function employeeSheduleAction(Request $request)
     {
-        $tomorrow = new \DateTime('tomorrow');
+        if ($request->isXmlHttpRequest()){
+            $date = new \DateTime($request->query->get('date'));
+//            echo($request->query->get('date'));die;
+        } else {
+            $date = new \DateTime('tomorrow');
+        }
+
 
         $this->em = $this->getDoctrine()->getManager();
 
         /** @var VisitDate[] $animalsVisitBook */
-        $animalsVisitBook = $this->em->getRepository(VisitDate::class)->findByDate($tomorrow);
+        $animalsVisitBook = $this->em->getRepository(VisitDate::class)->findByDate($date);
 
         /** @var EmployeesShedule[] $petsitterVisits */
-        $petsitterVisits = $this->em->getRepository(EmployeesShedule::class)->findByDate($tomorrow);
+        $petsitterVisits = $this->em->getRepository(EmployeesShedule::class)->findByDate($date);
 
         /** @var User[] $petsitters */
         $petsitters = $this->em->getRepository(User::class)->findBy([
@@ -38,10 +48,24 @@ class EmployeeSheduleController extends AdminController
         ]);
 
 //        dump($petsitter);die;
-        return $this->render('easy_admin/shedule.html.twig', [
-           'animalsVisitBook' => $animalsVisitBook,
-           'petsitterVisits' => $petsitterVisits,
-            'petsitters' => $petsitters
-        ])  ;
+        if ($request->isXmlHttpRequest()){
+            $animals = [];
+
+            foreach ($animalsVisitBook as $animalVisit) {
+                $animals[] = [
+                    'id'=>$animalVisit->getId(),
+                    'animalName'=>$animalVisit->getAnimal()->getName()
+                ];
+            }
+//            dump($animalsVisitBook);die;
+
+            return new JsonResponse($animals);
+        } else {
+            return $this->render('easy_admin/shedule.html.twig', [
+                'animalsVisitBook' => $animalsVisitBook,
+                'petsitterVisits' => $petsitterVisits,
+                'petsitters' => $petsitters
+            ])  ;
+        }
     }
 }
